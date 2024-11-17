f679fda2b20131ea8fb38d825804fd83778fcc33
Antonio Thomacelli Gomes <tonnytg@gmail.com>
2024-11-17 11:10:03 -0300

    feat: add token to add node

--- p1/scripts/worker/entrypoint.sh
+++ p1/scripts/worker/entrypoint.sh
@@ -4,6 +4,7 @@ sudo mkdir -p /home/vagrant/.ssh
 
 sudo cp /vagrant_data/iot_42 /home/vagrant/.ssh/iot_42
 sudo cp /vagrant_data/iot_42.pub /home/vagrant/.ssh/iot_42.pub
+sudo cp /vagrant_data/node-token /tmp/node-token
 sudo chmod 600 /home/vagrant/.ssh/iot_42
 
 cat /home/vagrant/.ssh/iot_42.pub >> /home/vagrant/.ssh/authorized_keys
@@ -11,8 +12,9 @@ cat /home/vagrant/.ssh/iot_42.pub >> /home/vagrant/.ssh/authorized_keys
 sudo chown -R vagrant:vagrant /home/vagrant/.ssh
 
 MASTER_IP=192.168.56.110
-
-TOKEN=$(ssh -i /home/vagrant/.ssh/iot_42 -o StrictHostKeyChecking=no vagrant@$MASTER_IP "sudo cat /var/lib/rancher/k3s/server/node-token")
+cp /vagrant_data/node-token /tmp/node-token
+TOKEN=$(cat /tmp/node-token)
+#TOKEN=$(ssh -i /home/vagrant/.ssh/iot_42 -o StrictHostKeyChecking=no vagrant@$MASTER_IP "sudo cat /var/lib/rancher/k3s/server/node-token")
 
 curl -sfL https://get.k3s.io | K3S_URL=https://$MASTER_IP:6443 K3S_TOKEN=$TOKEN sh -
 
